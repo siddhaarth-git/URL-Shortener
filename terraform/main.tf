be275e4ec87970ba5d93530b7e096a790787aaff
siddhaarth-git <siddhaarth.work@gmail.com>
2026-04-20 07:18:29 +0530

    add remote state backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform{
         }
     }
     required_version = ">=1.3.0"
+
+    backend "s3" {
+    bucket = "url-shortener-tfstate-sid"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws"{
